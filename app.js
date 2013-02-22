9d3cabe5b4cb610b3d6365ad89f41459679685bf
ybonnel <ybonnel@gmail.com>
2013-02-22 20:56:24 +0100

    Heart background

--- app.js
+++ app.js
@@ -85,5 +85,23 @@ angular.module('tutu', []).config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/all', {templateUrl: 'partial/all.html', controller: CodeStoryCtrl});
     $routeProvider.otherwise({redirectTo: '/all'});
 
-}])
+}]);
+
+
+$(function () {
+    var previousScrollTop, currentScrollTop, yPosition = 0;
+    $(window).scroll(function () {
+        currentScrollTop = $(window).scrollTop();
+        var diff = previousScrollTop - currentScrollTop;
+        if (isNaN(diff)) {
+            diff = 0;
+        }
+        yPosition = yPosition + diff;
+        previousScrollTop = currentScrollTop;
+        $('body').css('background-position', '0 ' + yPosition + 'px, ' + //
+            '0 ' + yPosition * 2 + 'px, ' + //
+            '0 ' + yPosition * 5 + 'px, ' + //
+            '0 0');
+    });
+});
 
